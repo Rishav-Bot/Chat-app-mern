f77ecd85977e104c5fd679ed2567b70a90c6f9e8
Rishav Gossain <rishavgossain007@gmail.com>
2023-11-23 23:40:43 +0530

    code splitting done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # chat-app-mern
+<!-- Performance Optimization(Code Splitting) is done by using lazy-suspense api of react. -->
--- public/src/App.js
+++ public/src/App.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Chat from './pages/Chat';
-import SetAvatar from './pages/SetAvatar';
 
+const Chat = lazy(() => import("./pages/Chat"));
+const Register = lazy(() => import("./pages/Register"));
+const Login = lazy(() => import("./pages/Login"));
+const SetAvatar = lazy(() => import("./pages/SetAvatar"));
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Suspense fallback={<></>}>
       <Routes>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/setAvatar" element={<SetAvatar />}></Route>
         <Route path="/" element={<Chat />}></Route>
       </Routes>
+    </Suspense>
     </BrowserRouter>
   );
 }
